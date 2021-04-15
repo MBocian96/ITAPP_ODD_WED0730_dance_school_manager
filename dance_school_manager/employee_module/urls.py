08f7f32c8ba20eb4bd9d4cddf062f7a29f75a72a
Bocian <michalxbocian@gmail.com>
2021-04-15 14:38:04 +0200

    templates organised

--- dance_school_manager/employee_module/urls.py
+++ dance_school_manager/employee_module/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from employee_module.views import EmployeeView
+from employee_module.views.main_view import EmployeeView
+from employee_module.views.manage_courses_view import ManageCoursesViews
 
 urlpatterns = [
     path('', EmployeeView.as_view(), name='employee_view'),
+    path('', ManageCoursesViews.as_view(), name='manage_courses_view'),
 ]
---
+++ dance_school_manager/employee_module/views/manage_courses_view.py
@@ -0,0 +1,8 @@
+# Create your views here.
+
+# Create your views here.
+from django.views.generic import CreateView
+
+
+class ManageCoursesViews(CreateView):
+    pass
