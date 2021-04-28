3a567803bd96027259ba73f010ed7a1ac60766ba
263232 <80218768+263232@users.noreply.github.com>
2021-04-28 21:09:19 +0200

    views' templates

---
+++ dance_school_manager/teacher_module/views/main_view.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render
+# Create your views here.
+from django.utils.decorators import method_decorator
+
+# Create your views here.
+from django.views.generic import CreateView
+
+
+class TeacherView(CreateView):
+    template = 'profiles/teacher/teacher_profile_view.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        context = {'manage_courses': 'course_names', # CHECK/ADD COURSES LIST OF THIS TEACHER HERE 
+                   'username': request.user.username,
+                   'avatar': request.user.avatar,
+                   }
+
+        return render(request, self.template, context=context)
