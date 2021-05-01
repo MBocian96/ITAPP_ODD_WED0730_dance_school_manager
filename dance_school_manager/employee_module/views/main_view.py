4c1d3319b9d601204e4cfd2fb4a6c3b9758b5bb4
Bocian <michalxbocian@gmail.com>
2021-05-01 17:21:54 +0200

    all courses printing

--- dance_school_manager/employee_module/views/main_view.py
+++ dance_school_manager/employee_module/views/main_view.py
@@ -1,22 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
-# Create your views here.
 from django.utils.decorators import method_decorator
-
-# Create your views here.
 from django.views.generic import CreateView
+
+from courses_module.models import Courses
 
 
 class EmployeeView(CreateView):
     template = 'profiles/employee/employee_profile_view.html'
+    context = {'manage_courses': 'manage_courses',
+               'manage_students': 'manage_students',
+               'manage_teachers': 'manage_teachers'
+               }
 
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):
-        context = {'manage_courses': 'manage_courses',
-                   'manage_students': 'manage_students',
-                   'manage_teachers': 'manage_teachers',
-                   'username': request.user.username,
-                   'avatar': request.user.avatar,
-                   }
+        self.context.update({'username': request.user.username,
+                             'avatar': request.user.avatar,
+                             })
 
-        return render(request, self.template, context=context)
+        self.context.update({'courses': Courses.objects.all()})
+
+        return render(request, self.template, context=self.context)
