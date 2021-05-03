c92e7af8876553be32702de417dd9475abe01278
Bocian <michalxbocian@gmail.com>
2021-05-03 15:51:23 +0200

    teacher and student refactor-inharitance

--- dance_school_manager/employee_module/views/teacher/manage_teachers_view.py
+++ dance_school_manager/employee_module/views/teacher/manage_teachers_view.py
@@ -1,19 +1,6 @@
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
-from django.utils.decorators import method_decorator
-
-from authentication_module.models import CustomUser
 from employee_module.views.employee_main_view import EmployeeView
 
 
 class ManageTeachersView(EmployeeView):
-    template_name = 'profiles/employee/teacher/manage_teachers_view.html'
-
-    @method_decorator(login_required)
-    def get(self, request, *args, **kwargs):
-        local_context = {'teachers': CustomUser.objects.filter(is_teacher=True),
-                         'username': request.user.username,
-                         'avatar': request.user.avatar,
-                         }
-        local_context.update(self.context)
-        return render(request, self.template_name, local_context)
+    template = 'profiles/employee/teacher/manage_teachers_view.html'
+    filter_arg = {'is_teacher': True}
