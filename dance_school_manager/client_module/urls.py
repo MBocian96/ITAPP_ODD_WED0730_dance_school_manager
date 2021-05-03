480c7c43730f8e46f2c52792a54bcce2913cc4a4
PureWater777 <peter9797@wp.pl>
2021-05-03 15:23:56 +0200

    add abandon course functionality

--- dance_school_manager/client_module/urls.py
+++ dance_school_manager/client_module/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from client_module.views import ClientView, ClientSettingsView
+from client_module import views
+from client_module.views import ClientView, ClientSettingsView, AbandonCourse
 
+app_name = 'client_module'
 urlpatterns = [
     path('', ClientView.as_view(), name='user_view'),
     path('settings/', ClientSettingsView.as_view(), name='user_settings_view'),
+    path('abandon/<int:pk>', AbandonCourse, name='abandon_course_view'),
 ]
--- dance_school_manager/client_module/views.py
+++ dance_school_manager/client_module/views.py
@@ -1,15 +1,16 @@
 from django.contrib.auth.decorators import login_required
+from django.http import request
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.views import View, generic
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DeleteView, UpdateView
 from django.contrib.auth.forms import UserChangeForm
-
-# Create your views here.
 from authentication_module.forms import EditProfileForm
+# Create your views here.
 
 
+# Client can view his courses with descriptions, hyperlink to settings
 class ClientView(CreateView):
     template_name = 'profiles/student/student_profile.html'
 
@@ -21,11 +22,38 @@ class ClientView(CreateView):
 
         return render(request, self.template_name, context=context)
 
-class ClientSettingsView(generic.UpdateView):
+
+
+# Client can edit parameters given in field
+class ClientSettingsView(UpdateView):
     form = UserChangeForm
     template_name = 'profiles/student/student_profile_settings.html'
-    success_url = reverse_lazy('user_view')
+    success_url = reverse_lazy('client_module:user_view')
     fields = {'email', 'username'}
 
     def get_object(self):
         return self.request.user
+
+
+
+#Client can leave course (deletes course from the database not just client's list, fuck)
+def AbandonCourse(request, pk):
+    user = request.user
+    to_delete = user.courses.get(id=pk)
+    context = {'item': to_delete}
+    if request.method == "POST":
+        to_delete.delete()
+        return redirect('client_module:user_view')
+
+    return render(request, 'profiles/student/abandon_course.html', context)
+
+
+
+
+
+
+
+
+
+
+
