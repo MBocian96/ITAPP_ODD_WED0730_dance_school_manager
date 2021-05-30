dd8b66a84a2fbc763f831d4d90fb36d750b948d8
263232 <263232@student.pwr.edu.pl>
2021-05-30 23:01:36 +0200

    scratch of teacher calendar

--- dance_school_manager/teacher_module/urls.py
+++ dance_school_manager/teacher_module/urls.py
@@ -4,11 +4,12 @@ from teacher_module.views.main_view import TeacherView
 from teacher_module.views.school_courses import SchoolCoursesViews
 from teacher_module.views.settings import SettingsViews
 from teacher_module.views.view_courses import CoursesListViews
+from teacher_module.views.calendar import CalendarViews
 app_name= "teacher_module"
 
 urlpatterns = [
     path('', TeacherView.as_view(), name='teacher_profile_view'),
     path('courses_list/', CoursesListViews.as_view(), name='courses_list_view'),
     path('settings/', SettingsViews.as_view(), name='settings_view'),
-   # path('school_courses', SchoolCoursesViews.as_view(), name='school_courses_view'),
+    path('calendar/', CalendarViews.as_view(), name='calendar_view'),
 ]
