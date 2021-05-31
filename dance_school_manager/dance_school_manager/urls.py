c5d46aadaa710a5c95c85da1b5dce0c78a0fd7da
Karol Kaluga <kaludzio@gmail.com>
2021-05-31 17:09:14 +0200

    employee module with front-end

--- dance_school_manager/dance_school_manager/urls.py
+++ dance_school_manager/dance_school_manager/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from authentication_module.views import redirect_to_login
 
@@ -24,3 +25,5 @@ urlpatterns = [
     path('authentication/', include('authentication_module.urls')),
     path('employee/', include('employee_module.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
