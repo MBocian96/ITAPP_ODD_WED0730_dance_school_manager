7f6bec9593ecdf8f38fbfa4af243c8f931953abb
263232 <263232@student.pwr.edu.pl>
2021-06-10 16:02:35 +0200

    teacher course board (applied courses and participants)

--- dance_school_manager/teacher_module/views/course_page.py
+++ dance_school_manager/teacher_module/views/course_page.py
@@ -11,17 +11,39 @@ from authentication_module.models import CustomUser
 from courses_module.models import Courses
 
 
+class CoursesViews(CreateView):
+    template_name = 'profiles/teacher/courses.html'
+
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+
+
+        context = {'username': request.user.username,
+                   'courses_list': request.user.courses.all()
+
+                   }
+
+        return render(request, self.template_name, context=context)
+
 class CoursePageViews(CreateView):
     template_name = 'profiles/teacher/course_page.html'
 
+    @method_decorator(login_required)
     def get(self, request, course_id: int):
         course = get_object_or_404(Courses, id=course_id)
         students_attended_to_this_course: QuerySet = CustomUser.objects.filter(courses__id=course_id)
 
-    @method_decorator(login_required)
-    def get(self, request, *args, **kwargs):
+        certain_course = Courses.objects.get(id=course_id)
+
+  #  @method_decorator(login_required)
+    #def get(self, request, *args, **kwargs):
+
+
         context = {'username': request.user.username,
                    'courses_list': request.user.courses.all(),
+                   'course_id': course_id,
+                   'certain_course' :certain_course,
+                   'students_attended': students_attended_to_this_course,
 
                    }
 
