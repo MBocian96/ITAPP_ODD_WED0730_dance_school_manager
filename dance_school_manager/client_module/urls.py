f896e937447d752698de239c41e9a65b33688fe4
Bocian <michalxbocian@gmail.com>
2021-06-14 20:57:18 +0200

    login required

--- dance_school_manager/client_module/urls.py
+++ dance_school_manager/client_module/urls.py
@@ -1,13 +1,15 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from client_module import views
 from client_module.views import ClientView, ClientSettingsView, AbandonCourse, CalendarView, ReportAbsenceView
+from employee_module.urls import login_url
 
 app_name = 'client_module'
 urlpatterns = [
-    path('', ClientView.as_view(), name='user_view'),
-    path('settings/', ClientSettingsView.as_view(), name='user_settings_view'),
-    path('abandon/<int:pk>', AbandonCourse, name='abandon_course_view'),
-    path('calendar/', CalendarView.as_view(), name='calendar_view'),
-    path('report_absence/', ReportAbsenceView.as_view(), name='report_absence_view'),
+    path('', login_required(ClientView.as_view(), login_url=login_url), name='user_view'),
+    path('settings/', login_required(ClientSettingsView.as_view(), login_url=login_url), name='user_settings_view'),
+    path('abandon/<int:pk>', login_required(AbandonCourse, login_url=login_url), name='abandon_course_view'),
+    path('calendar/', login_required(CalendarView.as_view(), login_url=login_url), name='calendar_view'),
+    path('report_absence/', login_required(ReportAbsenceView.as_view(), login_url=login_url),
+         name='report_absence_view'),
 ]
--- dance_school_manager/teacher_module/urls.py
+++ dance_school_manager/teacher_module/urls.py
@@ -1,21 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from teacher_module.views.create_message import MessagePostView
-from teacher_module.views.main_view import TeacherView
-from teacher_module.views.school_courses import SchoolCoursesViews
-from teacher_module.views.settings_view import SettingsViews
-from teacher_module.views.view_courses import CoursesListViews
+from employee_module.urls import login_url
 from teacher_module.views.calendar import CalendarViews
 from teacher_module.views.course_page import CoursePageViews, CoursesViews
+from teacher_module.views.create_message import MessagePostView
+from teacher_module.views.main_view import TeacherView
+from teacher_module.views.settings_view import SettingsViews
+from teacher_module.views.view_courses import CoursesListViews
 
 app_name = "teacher_module"
 
 urlpatterns = [
-    path('', TeacherView.as_view(), name='teacher_profile_view'),
-    path('courses_list/', CoursesListViews.as_view(), name='courses_list_view'),
-    path('settings/', SettingsViews.as_view(), name='settings_view'),
-    path('calendar/', CalendarViews.as_view(), name='calendar_view'),
-    path('courses/', CoursesViews.as_view(), name='courses_view'),
-    path('certain_course/<int:course_id>/', CoursePageViews.as_view(), name='course_page_view'),
-    path('certain_course/create_message/', MessagePostView.as_view(), name='create_message_view')
+    path('', login_required(TeacherView.as_view(), login_url=login_url), name='teacher_profile_view'),
+    path('courses_list/', login_required(CoursesListViews.as_view(), login_url=login_url), name='courses_list_view'),
+    path('settings/', login_required(SettingsViews.as_view(), login_url=login_url), name='settings_view'),
+    path('calendar/', login_required(CalendarViews.as_view(), login_url=login_url), name='calendar_view'),
+    path('courses/', login_required(CoursesViews.as_view(), login_url=login_url), name='courses_view'),
+    path('certain_course/<int:course_id>/', login_required(CoursePageViews.as_view(), login_url=login_url),
+         name='course_page_view'),
+    path('certain_course/create_message/', login_required(MessagePostView.as_view(), login_url=login_url),
+         name='create_message_view')
 ]
